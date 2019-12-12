091f5b5e8745a3c24a1de85e2de49b6a8cd4e3c8
sonders <778277514@qq.com>
2019-12-12 11:11:50 +0800

    修改信息相关页面

---
+++ pages/message/systemMessage/system.vue
@@ -0,0 +1,118 @@
+<template>
+<view>
+	 <view v-if="!isShowLoading">
+		 <view class="main uni-flex uni-column" v-for="(item,index) in list " :key="index">
+			  <view class="title_top uni-flex">
+				  <text class="title">{{item.title}}</text>
+				  <text class="date">{{item.date}}</text>
+			  </view>
+			  <view class="con_tent">
+				  <text>{{item.content}}</text>
+			  </view>
+			  
+		 </view>
+	 </view>
+	 <view class="show_404" v-if="show_404">
+	   <image src="../../../static/img/message/kong.png" mode="widthFix" style="width: 50px;height:50px"/>
+	   <text>数据没有呀</text>
+	   <button type="warn" size="mini" style="margin-top:15upx">重新加载</button>
+	 </view>
+	 <loading v-if="isShowLoading"></loading>
+</view>
+</template>
+
+<script>
+	import loading from "../../../components/loading-show/loading-show.vue";
+	export default{
+		components:{loading},
+		data(){
+			return{
+				isShowLoading:false,
+				show_404:null,
+				list:[
+					{
+						title:'整装优惠活动暂时下线公告',
+						date:'2019/10/15 12:00',
+						content:'尊敬的用户，为了给您带来更好的体验,整装优惠活动业务将于2019年12月26日22:00-2019年12月27日09:00期间进行系统升级，升级期间整装优惠活动入口将暂时下线。给您带来的不便，敬请谅解。感谢您一如既往的支持。'
+					},
+					{
+						title:'整装优惠活动暂时下线公告',
+						date:'2019/10/15 12:00',
+						content:'尊敬的用户，为了给您带来更好的体验,整装优惠活动业务将于2019年12月26日22:00-2019年12月27日09:00期间进行系统升级。'
+					}
+				]
+			}
+		},
+		onLoad() {
+			this.isShowLoading = true
+		},
+		mounted() {
+			setTimeout(()=>{
+				if(this.list.length != 0){
+					this.isShowLoading = false
+					this.show_404 = false
+				}else{
+					this.isShowLoading = false
+					this.show_404 = true
+				}
+			},1500)
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.main{
+	width: 100%;
+	height: auto;
+	border-bottom:6upx solid #EEEEEE;
+	padding: 20upx 0 40upx 0;
+	overflow: hidden;
+	.title_top{
+		width: 100%;
+		height: 80upx;
+		line-height: 80upx;
+		display: flex;
+		justify-content: space-between;
+		padding: 0 20upx;
+		.title{
+			font-size:30upx;
+			font-family:PingFang SC;
+			font-weight:400;
+			color:rgba(42,42,42,1);
+		}
+		.date{
+			font-size:24upx;
+			font-family:PingFang SC;
+			font-weight:400;
+			color:rgba(42,42,42,1);
+			opacity:0.5;
+			line-height:90upx;
+			padding-right:  40upx;
+		}
+	}
+	.con_tent{
+		width: 100%;
+		font-size:26upx;
+		font-family:PingFang SC;
+		font-weight:400;
+		color:rgba(42,42,42,1);
+		line-height:46upx;
+		opacity:0.7;
+		display: flex;
+		justify-content: center;
+		text{
+			width: 98%;
+			margin: 0 auto;
+		}
+	}
+}
+.show_404 {
+  width: 100%;
+  height: 400upx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 30upx;
+}
+</style>
---
+++ pages/message/systemMessage/userPick.vue
@@ -0,0 +1,123 @@
+<template>
+<view>
+	 <view v-if="!isShowLoading">
+		 <view class="main_hudong uni-flex" v-for="(item,index) in list " :key="index">
+			  <view class="left_main_view uni-flex">
+					<view  style="width:74upx;height: 74upx;">
+					  <image style="width: 100%;height: 100%;" :src='item.head_pic'/>
+					</view>
+					<view class="right_view uni-flex uni-column">
+						<text class="name">{{item.name}}</text>
+						<text class="name">{{item.set}}了您的作品</text>
+						<text class="time">{{item.time}}</text>
+					</view>
+			  </view>
+			  <view class="right_main_view">
+				  <image style="width:100%;height: 100%;" :src='item.img'/> 
+			  </view>
+		 </view>
+	 </view>
+	 <view class="show_404" v-if="show_404">
+	   <image src="../../../static/img/message/kong.png" mode="widthFix" style="width: 50px;height:50px"/>
+	   <text>数据没有呀</text>
+	   <button type="warn" size="mini" style="margin-top:15upx">重新加载</button>
+	 </view>
+	 <loading v-if="isShowLoading"></loading>
+</view>
+</template>
+
+<script>
+	import loading from "../../../components/loading-show/loading-show.vue";
+	export default{
+		components:{loading},
+		data(){	
+			return{
+				isShowLoading:true,
+				show_404:null,
+				list:[{
+					img:'../../../static/img/cjq-about/pcik.png',
+					head_pic:'../../../static/img/cjq-about/06.png',
+					name:'张三',
+					set:'评论',
+					time:'15:29'
+				},
+				{
+					img:'../../../static/img/cjq-about/pcik.png',
+					head_pic:'../../../static/img/cjq-about/02.png',
+					name:'李三',
+					set:'点赞',
+					time:'15:29'
+				},
+				{
+					img:'../../../static/img/cjq-about/pcik.png',
+					head_pic:'../../../static/img/cjq-about/05.png',
+					name:'王三',
+					set:'转发',
+					time:'15:29'
+				}
+				]
+			}
+		},
+		onLoad() {
+			this.isShowLoading = true
+		},
+		mounted() {
+			setTimeout(()=>{
+				if(this.list.length != 0){
+					this.isShowLoading = false
+					this.show_404 = false
+				}else{
+					this.isShowLoading = false
+					this.show_404 = true
+				}
+				
+			},1500)
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.main_hudong{
+	width: 100%;
+	height: 180upx;
+	justify-content: space-between;
+	align-items: center;
+	border-bottom: 1upx solid #e8e8e8;
+	.left_main_view{
+		width: auto;
+		height: 142upx;
+		margin-left: 40upx;
+		.right_view{
+			margin-left: 20upx;
+			justify-content: space-between;
+			.name{
+				font-size:30upx;
+				font-family:PingFang SC;
+				font-weight:400;
+				color:rgba(42,42,42,1);
+			}
+			.time{
+				font-size:24upx;
+				font-family:PingFang SC;
+				font-weight:400;
+				color:rgba(42,42,42,1);
+				opacity:0.5;
+			}
+		}
+	}
+	.right_main_view{
+		width: 142upx;
+		height: 142upx;
+		margin-right: 40upx;
+	}
+}
+.show_404 {
+  width: 100%;
+  height: 400upx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 30upx;
+}
+</style>
